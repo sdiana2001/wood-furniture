67f514a4957e426c30ef22e1a25d3faacd41f602
Sogdiana <sogdianaakmatova4@gmail.com>
2026-08-15 22:40:07 +0600

    First init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+  <Routes>
+      <Route path="/" element={<div>Главная страница</div>} />
+      <Route path="/catalog" element={<div>Каталог мебели</div>} />
+    </Routes>
+  )
+}
+
+export default App
